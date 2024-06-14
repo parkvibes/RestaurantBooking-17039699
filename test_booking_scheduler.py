2192da6cfe21290ac0adf744d6ebf1478df74c62
Hyunwoo Park <48782700+parkvibes@users.noreply.github.com>
2024-06-14 10:58:51 +0900

    Add files via upload

---
+++ test_booking_scheduler.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+class BookingSchedulerTest(unittest.TestCase):
+
+    def test_not_oclock(self):
+        pass
+
+    def test_oclock(self):
+        pass
+
+    def test_capacity_overflow(self):
+        pass
+
+    def test_normal(self):
+        pass
+
+    def test_normal_check_sms(self):
+        pass
+
+    def test_normal_check_email_without_email(self):
+        pass
+
+    def test__normal_check_email_with_email(self):
+        pass
+
+    def test_sunday(self):
+        pass
+
+    def test_non_sunday(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
