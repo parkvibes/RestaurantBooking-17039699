7a771e84cc5dcc5d00a51479a3ca11b6c2903549
Hyunwoo Park <dshw.park@samsung.com>
2024-06-14 11:39:37 +0900

    refactoring: extract contants

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -4,17 +4,25 @@ import sys
 import unittest
 from schedule import *
 from booking_scheduler import *
+
+
 def get_date_time(date: str):
     try:
         return datetime.strptime(date, '%Y-%m-%d %H:%M')
     except:
         raise ValueError('Invalid date format')
 
+
+TIME_NOT_OCLOCK = get_date_time('2021-01-01 12:30')
+TIME_OCLOCK = get_date_time('2021-01-01 12:00')
+CUSTOMER = Customer('Fake Name', '010-1234-5678')
+DEFAULT_CAPACITY = 3
+
+
 class BookingSchedulerTest(unittest.TestCase):
-    DEFAULT_CAPACITY = 3
 
     def setUp(self):
-        self.bs = BookingScheduler(BookingSchedulerTest.DEFAULT_CAPACITY)
+        self.bs = BookingScheduler(DEFAULT_CAPACITY)
         self.backup_stdout = sys.stdout
         self.stdout = io.StringIO()
         sys.stdout = self.stdout
@@ -22,24 +30,13 @@ class BookingSchedulerTest(unittest.TestCase):
     def tearDown(self):
         sys.stdout = self.backup_stdout
 
-
-
     def test_not_oclock(self):
-        not_oclock_time = get_date_time('2021-01-01 12:30')
-        customer = Customer('Fake Name', '010-1234-5678')
-
         with self.assertRaises(ValueError):
-            self.bs.add_schedule(Schedule(not_oclock_time, 2, customer))
+            self.bs.add_schedule(Schedule(TIME_NOT_OCLOCK, DEFAULT_CAPACITY - 1, CUSTOMER))
 
     def test_oclock(self):
-        oclock_time = get_date_time('2021-01-01 12:00')
-        customer = Customer('Fake Name', '010-1234-5678')
-
-        self.bs.add_schedule(Schedule(oclock_time, 2, customer))
-
+        self.bs.add_schedule(Schedule(TIME_OCLOCK, DEFAULT_CAPACITY - 1, CUSTOMER))
         self.assertEqual(self.stdout.getvalue(), "Sending SMS to 010-1234-5678 for schedule at 2021-01-01 12:00:00\n")
-
-
 
     def test_capacity_overflow(self):
         pass
